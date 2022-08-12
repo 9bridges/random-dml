b49a230dfc1be88dc051a39ba0d0c47e93007b8e
hbk671104 <hbk671104@gmail.com>
2022-08-12 14:45:33 +0800

    completed

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import knex from 'knex'
 import { faker } from '@faker-js/faker'
 
-const INITIAL_NUMBER = 100
-const INCREMENTAL_NUMBER = 100
-
 const knexClient = knex({
     client: 'mysql',
     connection: {
@@ -16,16 +13,51 @@ const knexClient = knex({
 })
 
 ;(async () => {
+    let INITIAL_NUMBER = 100
+    const INCREMENTAL_NUMBER = 20
+    const TABLE_NAME = 'test'
+
     try {
-        const res = await knexClient.batchInsert(
-            'test',
+        // Insert a certain number of records
+        await knexClient.batchInsert(
+            TABLE_NAME,
             [...Array(INITIAL_NUMBER).keys()].map((n) => ({
                 id: n + 1,
                 name: faker.name.firstName(),
             }))
         )
-        console.info(res)
+
+        // Perform random DML operations
+        await Promise.all(
+            [...Array(INCREMENTAL_NUMBER).keys()].map(async () => {
+                const randomNumber =
+                    Math.floor(Math.random() * INITIAL_NUMBER) + 1
+                const randomOperation = Math.floor(Math.random() * 3)
+
+                switch (randomOperation) {
+                    case 0:
+                        INITIAL_NUMBER += 1
+                        return await knexClient(TABLE_NAME).insert({
+                            id: INITIAL_NUMBER,
+                            name: faker.name.firstName(),
+                        })
+                    case 1:
+                        return await knexClient(TABLE_NAME)
+                            .update({
+                                name: `${faker.name.firstName()}（已改）`,
+                            })
+                            .where({ id: randomNumber })
+                    case 2:
+                        return await knexClient(TABLE_NAME)
+                            .delete()
+                            .where({ id: randomNumber })
+                }
+            })
+        )
+
+        process.exit(0)
     } catch (error) {
         console.error(error)
+        process.exit(1)
     }
 })()
