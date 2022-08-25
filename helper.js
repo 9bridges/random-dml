9d54c6d7af690db3957fc11b871dbe6754416f4a
hbk671104 <hbk671104@gmail.com>
2022-08-25 19:23:21 +0800

    add decimal/numeric/text/char/mediumtext handling

--- helper.js
+++ helper.js
@@ -4,16 +4,27 @@ export const generateRandomRow = () =>
     global.COLUMN_INFO.reduce((acc, column) => {
         const { name, data_type, is_primary_key, default_value } = column
         if (is_primary_key) return acc
-        if (default_value) return acc
+        // if (default_value) return acc
 
         let value
         switch (data_type) {
             case 'int':
-                value = faker.datatype.number()
+            case 'decimal':
+            case 'numeric':
+                const { numeric_precision, numeric_scale } = column
+                value = faker.datatype.number({
+                    max: 10 ** numeric_precision,
+                    precision: 1 / 10 ** numeric_scale,
+                })
+                break
             case 'varchar':
+            case 'char':
+            case 'text':
+            case 'mediumtext':
                 const { max_length } = column
                 // 10 is default length
                 value = faker.datatype.string(max_length < 10 ? max_length : 10)
+                break
         }
 
         return {
